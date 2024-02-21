161ccb41c6ff601a301ef05caed5c288c1b1cdad
Gabriel <85773507+GabrielG-PWD@users.noreply.github.com>
2024-02-21 14:14:23 -0300

    seccion barras terminado

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -12,26 +12,16 @@ export class BarrasComponent {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   public barChartOptions: ChartConfiguration['options'] = {
-    scales: {
-      x: {},
-      y: {
-        min: 10,
-      },
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-    },
+    responsive: true,
   };
 
   public barChartType: ChartType = 'bar';
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['2018', '2019', '2020', '2021', '2022', '2023', '2024'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#FF8FD6', hoverBackgroundColor: '#460350' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#FF595F', hoverBackgroundColor: '#460350' },
     ],
   };
 
@@ -40,17 +30,13 @@ export class BarrasComponent {
 
 
   public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40,
-    ];
 
+    this.barChartData.datasets.forEach((set, i) => {
+      set.data.forEach((d, j) => {
+        d = Math.round(Math.random() * 100);
+        this.barChartData.datasets[i].data[j] = d
+      })
+    })
     this.chart?.update();
   }
 }
