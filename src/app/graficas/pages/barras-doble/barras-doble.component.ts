b06743a658575d4bf89fb85f77e48363f55e757d
Gabriel <85773507+GabrielG-PWD@users.noreply.github.com>
2024-02-21 11:25:34 -0300

    Agregando estructura del proyecto

---
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barras-doble',
+  templateUrl: './barras-doble.component.html',
+  styles: ``
+})
+export class BarrasDobleComponent {
+
+}
